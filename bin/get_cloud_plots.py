81e6b14d1bfcd8c0677152538d7fa5292888e2d3
Tobias Preis <tobias.preis@uibk.ac.at>
2026-02-24 21:21:41 +0100

    Fix showing plot

--- bin/get_cloud_plots.py
+++ bin/get_cloud_plots.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python
 from astronacht_meteo.astronacht import Astronacht
+import matplotlib.pyplot as plt
 
 if __name__ == "__main__":
     astronacht = Astronacht()
-    fig, _ = astronacht.get_cloud_plot()
-    fig.show()
+    fig, ax = astronacht.get_cloud_plot()
+    plt.show()
