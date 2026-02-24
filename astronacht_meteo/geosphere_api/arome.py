5b237ec272cef431d00dfa97bba5e9f8e6c03ed6
Tobias Preis <tobias.preis@uibk.ac.at>
2026-02-24 18:34:35 +0100

    add get_timeseries functionality

--- astronacht_meteo/geosphere_api/arome.py
+++ astronacht_meteo/geosphere_api/arome.py
@@ -1,4 +1,6 @@
 from .geosphere_hub import GeosphereAPI
+from datetime import datetime, timedelta
+import numpy as np
 
 
 class AROME(GeosphereAPI):
@@ -7,4 +9,22 @@ class AROME(GeosphereAPI):
 
     def __init__(self, dtype="timeseries"):
         self.dtype = dtype
+        self._last_query = {}
         super().__init__()
+
+    def get_timeseries_data(
+        self,
+        parameters=["tcc"],
+        position="47.26435136748764,11.342491131078917",  # VFH
+    ):
+        params = {
+            "parameters": parameters,
+            "lat_lon": position,
+            "forecast_offset": 0,
+        }
+
+        res = self._query_dict(params)
+        self._last_query[str(params.items())] = res
+        self._data = np.array(res["data"], dtype=float)
+        self._times = res["timestamps"]
+        return str(params.items())
