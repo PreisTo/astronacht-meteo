bdaedae464dbd6947648c596759573f214b8cbb3
Tobias Preis <tobias.preis@uibk.ac.at>
2026-02-25 15:36:07 +0100

    Add template conditions

---
+++ astronacht_meteo/conditions.py
@@ -0,0 +1,19 @@
+class Conditions:
+    def _clouds(self):
+        pass
+
+    def _percipitation(self):
+        pass
+
+    def _sun(self):
+        pass
+
+    def _thunderstorm_risk(self):
+        pass
+
+    def _risk(self):
+        pass
+
+
+class MinimalConditions(Conditions):
+    pass
