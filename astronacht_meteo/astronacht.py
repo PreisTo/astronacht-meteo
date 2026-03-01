00151f4ca3e57869d0325e2b7e1f437775d127d2
Tobias Preis <tobias.preis@uibk.ac.at>
2026-03-01 20:40:25 +0100

    Refactor use new weather class in astronacht

--- astronacht_meteo/astronacht.py
+++ astronacht_meteo/astronacht.py
@@ -1,8 +1,7 @@
 from typing import Union
 from .date import Date
 from .location import Location
-from astronacht_meteo.geosphere_api.arome import AROME
-from .targets.targets import GoodTargets
+from astronacht_meteo.weather import Weather
 from astronacht_meteo.utils.config import load_config
 from pathlib import Path
 
@@ -12,18 +11,16 @@ class Astronacht:
     The Astronacht itself.
     It holds all the relevant functions and objects like date and location
 
-    It'll query the weather APIs to get the forecast
+    It'll e.g. query the weather APIs to get the forecast
     """
 
     def __init__(self, date=None, location="Technik") -> None:
-        self._arome = AROME()
+        self._weather = Weather(location)
 
     @classmethod
     def from_config(cls, config_file: Union[Path, str]):
         config_dict = load_config(config_file)
-        date = Date.from_config(config_dict["date"])
         location = Location.from_dict(config_dict["location"])
-        if config_dict["targets"]["auto"]:
-            good_targets = GoodTargets.from_dict()
+        date = Date.from_config(config_dict["date"], location)
 
         return cls(date=date, location=location)
