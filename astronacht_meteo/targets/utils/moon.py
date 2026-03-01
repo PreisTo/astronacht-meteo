5ce4251e528ee66b286b6b61cc17c7b069971cd8
Tobias Preis <tobias.preis@uibk.ac.at>
2026-03-01 20:38:54 +0100

    Fix moon separation function

--- astronacht_meteo/targets/utils/moon.py
+++ astronacht_meteo/targets/utils/moon.py
@@ -1,10 +1,13 @@
-from astropy.coordinates import get_body, SkyCoord
+from astropy.coordinates import get_body
+import numpy as np
+import astropy.units as u
+
 MOON_RADIUS = 1737.4 * u.km
 
-def separation_to_moon(observer, time):
-    moon = get_body("moon", time, obesrver.location)
+
+def separation_to_moon(observer, time, target):
+    moon = get_body("moon", time, observer.location)
     moon_distance = moon.distance
     moon_ang_radius = np.arcsin(MOON_RADIUS / moon_distance).to(u.deg)
     separation = moon.separation(target.transform_to(moon.frame)) - moon_ang_radius
     return separation
-
