1ee7c710a629ff705cd6f6a1ef3b278aed7bc0ae
Tobias Preis <tobias.preis@uibk.ac.at>
2026-03-01 20:41:30 +0100

    Add start working on date class

--- astronacht_meteo/date.py
+++ astronacht_meteo/date.py
@@ -1,4 +1,24 @@
+from .location import Location
+from datetime import datetime
+import pytz
+from typing import Optional
+
+
 class Date:
-    @classmethod
-    def from_dict(cls, config_dict: dict):
+    def __init__(self, day: datetime, start_time, duration, stop_time):
         pass
+
+    @classmethod
+    def from_dict(cls, config_dict: dict, location: Location):
+        if config_dict["day"] == "today":
+            day = datetime.today()
+        elif config_dict["day"] == "tomorrow":
+            day = datetime.day
+        else:
+            try:
+                day = datetime.strptime("%d.%m.%y", config_dict["day"])
+            except Exception as e:
+                raise TypeError(
+                    "The day must be either today, tomorrow or a date specified by dd.mm.yy"
+                ) from e
+        return cls(day)
