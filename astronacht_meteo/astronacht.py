4c5ccdc407fc341ca3a9b362d5634817e1140908
tobi <tobi@fedora.fritz.box>
2026-03-04 20:43:04 +0100

    add: check and assign location and date

--- astronacht_meteo/astronacht.py
+++ astronacht_meteo/astronacht.py
@@ -7,6 +7,13 @@ from astronacht_meteo.weather import Weather
 from .date import Date
 from .location import Location
 
+_known_locations = {
+    "technik": Location(
+        lon=11.342491131078917, lat=47.26435136748764, name="Technik", elevation=600
+    ),
+    "reith": Location(lat=47.300188, lon=11.203271, name="Reith", elevation=1140),
+}
+
 
 class Astronacht:
     """
@@ -19,11 +26,30 @@ class Astronacht:
     def __init__(self, date=None, location="Technik") -> None:
         self._weather = Weather(location)
 
-    def _check_location(self, location):
-        pass
+    def _check_location(self, location: Union[Location, str]) -> Location:
+        if isinstance(location, Location):
+            return location
+        elif isinstance(location, str):
+            if location.lower() in _known_locations.keys():
+                return _known_locations[location.lower()]
+            else:
+                raise ValueError(
+                    f"{location} is not a known location... "
+                    + f"I know about {_known_locations.keys()}"
+                )
+        else:
+            raise NotImplementedError(
+                "You need to provide location as either Location or str"
+            )
 
-    def _check_date(self, date):
-        pass
+    def _check_date(self, date: Union[Date, str]) -> Date:
+        if isinstance(date, Date):
+            return date
+        elif isinstance(date, str):
+            # TODO: implement from string in Date
+            raise NotImplementedError("This is a todo... need to handle that in Date")
+        else:
+            raise NotImplementedError("You need to provide date as either Date or str")
 
     @classmethod
     def from_config(cls, config_file: Union[Path, str]):
