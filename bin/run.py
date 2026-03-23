b21624dbf098c284efc629110e447f4b57f36b89
Tobias Preis <tobias.preis@uibk.ac.at>
2026-03-23 22:22:44 +0100

    chore: test on tomorrow

--- bin/run.py
+++ bin/run.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 
-from astronacht_meteo.astronacht import Astronacht
 import matplotlib.pyplot as plt
+
+from astronacht_meteo.astronacht import Astronacht
 
 if __name__ == "__main__":
     astronacht = Astronacht.from_config("./default_config.yml")
